0e4d31276f0c500c6edaa7f9cdbbf33a576c256c
Mari Moreira <107552602+marizinhart@users.noreply.github.com>
2024-08-09 08:46:20 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,89 @@
+ export const perguntas = [
+    {
+        enunciado:"Apos você sair da escola, tu se depara com uma tecnologia nova, isto é:",
+        alternativas: [
+            {
+                 texto: "Surpriendente!",
+                 afirmacao: [
+                 "A tecnologia veio para facilitar nossa vida,",
+                "Como uma forma de melhoração,"
+                ]
+            },
+          {
+             texto: "horrível!",
+             afirmacao:[ "A tecnlogia é um problema nos dias de hoje,",
+             "Não vai facilitar a vida de ninguém"
+          ]
+          }
+        ]
+    },
+    {
+        enunciado: "O que é uma ingteligencia artificial?",
+        alternativas: [
+            {
+                texto:  "É a capacidade que uma máquina tem para produzir competências semelhantes ás humanas como raciocínio!",
+                afirmacao: [ "nos auxiliando em tudo que precisamos,",
+                  "vai nos proporcionar melhoramento,"
+            ]
+            },
+            {
+                texto:  "É uma capacidade que não trás benefício nenhum",
+                afirmacao: [ "ela nos tira a concentração,",
+                   "não nos deixa ficar focados no que vamos fazer,"
+                ]
+            }
+        ]
+    },
+    {
+        enunciado: "Onde é usada essa tecnologia?",
+        alternativas: [
+            {
+                texto: "Em sistemas de reconhecimento de imagem, como placas e etc!",
+
+                afirmacao: [ "alem de ser uma forma mais pratica e facil de se conectar.",
+                 "sera muito mais facil de se conectar,"
+                ]
+            },
+            {
+                texto: "Não é usada em nada!",
+                afirmacao: [  "nos causam problemas na saude, entre outros.",
+                 "vai nos trazer malefícios para nosso bem estar,"
+                ]
+            }
+        ]
+    },
+    {
+        enunciado: "O que ela pode fazer?",
+        alternativas: [
+            {
+                texto: "Expressar emoções, empatia e ter sentimentos!",
+                afirmacao: [ "Não vai trazer nenhum beneficio.",
+                "não teremos boa comunicação."
+            ]
+            },
+            {
+                texto: "Avaliar dados, analisar imagens, ajudar em trabalhos!",
+                afirmacao:  [ "Resumindo a tecnologia veio para facilitar nossa vida.",
+                "além de nos auxiliar nos estudos."
+            ]
+            }
+        ]
+    },
+    {
+        enunciado:"Como ela pode mudar a maneira como trabalhamos?",
+        alternativas: [
+            {
+                texto: "Ela não interfere em nada!",
+                afirmacao: [ "Resumindo a tecnologia veio para atrasar nossa vida.",
+                "A tecnologia veio nos desconectar do mundo real."
+            ]
+            },
+            {
+                texto: "Nos ajudando a fazer um trabalho, orgânizando nossos horarios!",
+                afirmacao: [ "A tecnologia é um grande melhoramento de vida.",
+                "Portanto a tecnologia é uma ferramenta estremamente utíl."
+            ]
+            }
+        ]
+    },
+];
